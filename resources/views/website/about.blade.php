2fc3b5784ca953693249fa9c33c66bc555448720
jobysooraj <jobychacko14@gmail.com>
2025-03-18 10:46:28 +0530

    about us page changes

--- resources/views/website/about.blade.php
+++ resources/views/website/about.blade.php
@@ -25,11 +25,15 @@
             <div class="row gy-5">
                 <div class="col-xl-12" data-aos="fade-up" data-aos-delay="200">
                     <div class="about-content ">
+                        @if (!empty($aboutuses) && isset($aboutuses[0]))
                         <img src="{{ asset('storage/' . $aboutuses[0]->image) }}" class="img-fluid w-100 mb-4" alt="{{ $aboutuses[0]->title }}">
-                        <h3 class="mb-3">{{ $aboutuses[0]->title }}</h3>
+                        <h3 class="mb-3">{{ $aboutuses[0]->title ?? '' }}</h3>
                         <div class="about-description">
                             {!! $aboutuses[0]->description !!}
                         </div>
+
+                        @endif
+
                     </div>
                 </div>
             </div>
